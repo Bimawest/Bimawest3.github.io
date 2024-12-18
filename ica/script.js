caf815bdd793ec4e76540cf072d18206ea207afb
Bimawest <bimarizki55@gmail.com>
2024-12-19 00:52:11 +0800

    Add files via upload

---
+++ ica/script.js
@@ -0,0 +1,5 @@
+function enableScroll() {
+    var heroSection = document.getElementById('hero');
+    heroSection.style.display = 'none'; // Menghilangkan section hero
+    document.body.style.overflow = 'auto'; // Mengizinkan scroll pada body
+}
